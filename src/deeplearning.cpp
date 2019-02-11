5113dcd0f5599f07d8816054efdcca32e7345f2f
DougChenli <cchenli@nd.edu>
2019-02-11 16:42:18 -0500

    dl.cpp .h are added

---
+++ src/deeplearning.cpp
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "deeplearning.h"
+
+#include "fstream"
+#include "iostream"
+#include "stdio.h"
+#include "string.h"
+
+
+
